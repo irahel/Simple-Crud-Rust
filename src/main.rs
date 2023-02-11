4aaf8e2c145e4290edf13a1b047abb4e57c92ca3
Irahel <isaac_rahel@live.com>
2023-02-11 11:00:24 -0300

    feat: implements base api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get, http::Status, serde::json::Json};
+use serde::Serialize;
+
+#[macro_use] extern crate rocket;
+
+#[derive(Serialize)]
+pub struct GenericResponse {
+    pub status: String,
+    pub message: String,
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/healthchecker")]
+pub async fn health_checker_handler() -> Result<Json<GenericResponse>, Status> {
+    const MESSAGE: &str = "Build Simple CRUD API with Rust and Rocket";
+
+    let response_json = GenericResponse {
+        status: "success".to_string(),
+        message: MESSAGE.to_string(),
+    };
+    Ok(Json(response_json))
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, hello, health_checker_handler])
 }
